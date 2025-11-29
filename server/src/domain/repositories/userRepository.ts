6e98bfc0991ea84e1bd7bfd202b5905292bf3ab5
alsadir00 <alsadir00@gmail.com>
2025-11-29 11:36:43 +0300

    User repository is added

---
+++ server/src/domain/repositories/userRepository.ts
@@ -0,0 +1,10 @@
+import { User } from "@domain/entities/user";
+
+export interface UserRepository {
+    createUser(user: User): Promise<User>;
+    findUserByEmail(email: string): Promise<User | null>;
+    findUserById(id: string): Promise<User | null>;
+    getAllUsers(): Promise<User[]>;
+    updateUser(id: string, data: Partial<User>): Promise<User | null>;
+    deleteUser(id: string): Promise<void>;
+}
